27e0acfe437181d66726506291737dbc162e9601
rmnaser90 <rmnaser90@gmail.com>
2020-12-23 16:56:02 +0200

    all done need to works on handlebars

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import Renderer from './Renderer'
+import APIManager from'./APIManager'
+import './style.css'
+import $ from 'jquery'
+
+
+
+const renderer = new Renderer()
+const apiManager = new APIManager()
+
+const loadData = () => apiManager.loadData()
+const renderData = () => renderer.render(apiManager.data)
+
+$('#load-btn').on('click', loadData)
+$('#display-btn').on('click', renderData)
